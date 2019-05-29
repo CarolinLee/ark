ad26075cb80ca3ad8f3ef051f8d21283ec4e66eb
CarolinLee <Adriemelon@163.com>
2019-05-29 19:22:04 +0800

    home.vue卡住不动

---
+++ SpringBoot-Shiro-Vue-master/vue/src/components/headerDemo/HomeHeader.vue
@@ -0,0 +1,49 @@
+<template lang = "jade">
+  -var c = 'homeHeader'
+  -var wrap = c + '__wrap'
+
+  div(class="#{wrap}")
+    div(class="logo")
+      img(src='../../common/img/logo/GTSlogo.jpg' width="226" height="48")
+      | &nbsp;&nbsp;&nbsp;&nbsp;
+      span Ark Regulation Database
+      | &nbsp;&nbsp;&nbsp;
+      img(src='../../common/img/logo/copslogo.jpg' width="50" height="55")
+
+</template>
+
+<script>
+  export default {
+    name: "homeHeader",
+    data() {
+      return {
+      }
+    },
+    methods: {
+    },
+  }
+</script>
+
+<style>
+  @c homeHeader {
+    @d wrap {
+      .logo {
+        img {
+          padding-top: 10px;
+          /*width: 226px;*/
+          /*height: 48px;*/
+          vertical-align:bottom;
+        }
+        span {
+          background-color: rgba(255, 102, 0, 1);
+          white-space: nowrap;
+          font-family: 'Arial Bold', 'Arial Regular', 'Arial';
+          font-weight: 700;
+          font-style: normal;
+          font-size: 40px;
+        }
+      }
+
+    }
+  }
+</style>
---
+++ SpringBoot-Shiro-Vue-master/vue/src/components/searchDemo/SearchOne.vue
@@ -0,0 +1,81 @@
+<template lang = "jade">
+  -var c = 'searchOne'
+  -var wrap = c + '__wrap'
+
+  div(class="#{wrap}")
+    div(class="searchOption")
+      label 政策名称：
+      input(class="searchText", v-model="searchStr")
+      &nbsp&nbsp&nbsp&nbsp
+      //img(class="cancel", src="../../common/img/icon/cancel.png", @click="cancel()")
+      label 政策代号：
+      input(class="searchText", v-model="searchStr")
+      &nbsp&nbsp&nbsp&nbsp
+      label 行政部门：
+      input(class="searchText", v-model="searchStr")
+      &nbsp&nbsp&nbsp&nbsp
+      label 风险等级：
+      input(class="searchText", v-model="searchStr")
+      &nbsp&nbsp&nbsp&nbsp
+      label 业务影响：
+      input(class="searchText", v-model="searchStr")
+      div(class="searchBtn") 搜索
+        br
+        | Search
+
+
+</template>
+
+<script>
+  export default {
+    name: "searchOne",
+    data() {
+      return {
+        searchStr: '',
+      }
+    },
+    methods: {
+      cancel() {
+        this.searchStr = '';
+      }
+    },
+  }
+</script>
+
+<style>
+  @c searchOne {
+    @d wrap {
+      .searchOption{
+        width: 1300px;
+        position: relative;
+        overflow: hidden;
+        padding-top: 23px;
+        margin: 0 54px;
+        .searchText {
+          width: 130px;
+          height: 35px;
+        }
+        .cancel {
+          position: absolute;
+          right: 125px;
+          bottom: 2px;
+          &:hover{
+            cursor: pointer;
+           }
+       }
+        .searchBtn {
+          float: right;
+          width: 110px;
+          height: 39px;
+          font-size: 1em;
+          font-weight: bold;
+          color: white;
+          background-color: #169BD5;
+          &:hover{
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+</style>
